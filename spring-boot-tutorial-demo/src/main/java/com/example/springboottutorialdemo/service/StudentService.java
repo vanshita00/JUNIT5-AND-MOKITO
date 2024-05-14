57f52fcd783d4b2e336bc8f8f3c2e59e80607740
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2024-05-14 16:08:30 +0530

    Add files via upload

---
+++ spring-boot-tutorial-demo/src/main/java/com/example/springboottutorialdemo/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.springboottutorialdemo.service;
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+import com.example.springboottutorialdemo.entity.StudentEntity;
+
+public interface StudentService {
+    StudentEntity addStudent(StudentEntity student);
+
+    List<StudentEntity> getAllStudent();
+
+    StudentEntity getStudentById(Integer id);
+
+    StudentEntity updateStudentById(@Valid StudentEntity student, Integer id);
+
+    StudentEntity deleteStudentById(Integer id);
+
+    List<StudentEntity> getStudentByName(String name);
+
+}
+
